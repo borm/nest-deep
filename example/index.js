229a8fcea00bd73d145a1d73844e96a8994fcc4e
borm <efim4eg@gmail.com>
2019-08-23 13:54:05 +0300

    update README

--- example/index.js
+++ example/index.js
@@ -1,22 +1,31 @@
 import serialize from '../src/serialize';
 import deserialize from '../src/deserialize';
 
-const render = (object, id) => {
-  document.getElementById(id).innerHTML = JSON.stringify(object, undefined, 2);
-};
+const render = (object, id) =>
+  (document.getElementById(id).innerHTML = JSON.stringify(
+    object,
+    undefined,
+    2
+  ));
 
-render(serialize({
-  'a.b': 1,
-  'c.d[0][0]': 1,
-  'c.d[1]': 5,
-  'c.d[2]': 12,
-}), 'serialize');
+render(
+  serialize({
+    'a.b': 1,
+    'c.d[0][0]': 1,
+    'c.d[1]': 5,
+    'c.d[2]': 12,
+  }),
+  'serialize'
+);
 
-render(deserialize({
-  a: {
-    b: 1,
-  },
-  c: {
-    d: [[1], 5, 12],
-  }
-}), 'deserialize');
+render(
+  deserialize({
+    a: {
+      b: 1,
+    },
+    c: {
+      d: [[1], 5, 12],
+    },
+  }),
+  'deserialize'
+);
